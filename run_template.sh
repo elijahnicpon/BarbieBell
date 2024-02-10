99eb36558e478f3adba0b6320f264cf5407411de
Elijah Nicpon <elija@Elijahs-MacBook-Pro-10.local>
2024-02-10 01:26:34 -0500

    init streamlit, requirements, sh script

---
+++ run_template.sh
@@ -0,0 +1,3 @@
+export OPENAI_API_KEY="" # your openai key here
+export ATLAS_CONNECTION_STRING="mongodb+srv://<user>:<password>@vectors.qae9czv.mongodb.net/?retryWrites=true&w=majority"
+streamlit run 🏋️‍♀️_barbiebell.py
